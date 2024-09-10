38a44131768f022cfec422550b1c42cbb3dc7b5a
gree1ka <sasha.vlasov.312@gmail.com>
2024-09-10 14:53:30 +0300

    google collab setup

--- testissue/settings.py
+++ testissue/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-1=!y8peinwkk%1*imud5l6az9xt387p%theww1!d8=gzinwg=#
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['.ngrok-free.app', 'localhost', '127.0.0.1']
 
 
 # Application definition
